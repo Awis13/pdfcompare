86f5ff3f3009e6e2ae422fc4cfaadea5ddaeaadb
Nipostni <awis@me.com>
2023-03-17 21:28:32 +0100

    comments

--- compare_pdfs.py
+++ compare_pdfs.py
@@ -2,6 +2,7 @@ import sys
 import fitz  # PyMuPDF
 import difflib
 
+# Function to extract text from a PDF file
 def extract_text_from_pdf(file_path):
     doc = fitz.open(file_path)
     text = ""
@@ -9,12 +10,13 @@ def extract_text_from_pdf(file_path):
         text += page.get_text()
     return text
 
-
+# Function to compare two texts and return a list of differences
 def compare_texts(text1, text2):
     d = difflib.Differ()
     diff = list(d.compare(text1.splitlines(), text2.splitlines()))
     return diff
 
+# Function to display the differences in a readable format
 def display_diff(diff):
     for line in diff:
         if line.startswith('- '):
@@ -24,15 +26,22 @@ def display_diff(diff):
         else:
             print(line)
 
+# Main function
 if __name__ == "__main__":
+    # Check if the correct number of arguments are provided
     if len(sys.argv) != 3:
         print("Usage: python compare_pdfs.py file1.pdf file2.pdf")
         sys.exit(1)
 
+    # Get the file paths from command line arguments
     file1, file2 = sys.argv[1], sys.argv[2]
 
+    # Extract text from the PDF files
     text1 = extract_text_from_pdf(file1)
     text2 = extract_text_from_pdf(file2)
 
+    # Compare the extracted texts
     diff = compare_texts(text1, text2)
+
+    # Display the differences
     display_diff(diff)
